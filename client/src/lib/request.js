1968649441af22d60c69b0959a0743aa8401f59c
Violeta Kalcheva <violetakalcheva@abv.bg>
2024-01-08 19:50:20 +0200

    Upgrade requester

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,7 +1,63 @@
-export const request = async (method, url, data) => {
+// First
+// export const request = async (method, url, data) => {
+//     const response = await fetch(url, {
+//         method,
+//     });
+//     const result = await response.json();
+//     return result;
+// };
+
+// Second Option
+// const buildOptions = (data) => {
+//     const options = {};
+
+//     if (data) {
+//         options.body = JSON.stringify(data);
+//         options.headers = {
+//             'content-type': 'application/json'
+//         };
+
+//     }
+//     return options;
+// };
+
+// export const request = async (method, url, data) => {
+//     const response = await fetch(url, {
+//         ...buildOptions(data),
+//         method,
+//     });
+
+//     const result = await response.json();
+//     return result;
+// };
+
+// Third option
+
+const buildOptions = (data) => {
+    const options = {};
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            'content-type': 'application/json'
+        };
+
+    }
+    return options;
+};
+
+const request = async (method, url, data) => {
     const response = await fetch(url, {
+        ...buildOptions(data),
         method,
     });
+
     const result = await response.json();
     return result;
 };
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const remove = request.bind(null, 'DELETE');
+export const patch = request.bind(null, 'PATCH');
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,23 +1,59 @@
-import { request } from '../lib/request';
+// First
+
+// import { request } from '../lib/request';
+
+// const baseUrl = 'http://localhost:3030/jsonstore/games'
+
+// export const getAll = async () => {
+//     const result = await request('GET', baseUrl);
+//     return Object.values(result);
+
+// };
+
+// export const create = async (gameData) => {
+//     const response = await fetch(baseUrl, {
+//         method: 'POST',
+//         headers: {
+//             'content-type': 'application/json'
+//         },
+//         body: JSON.stringify(gameData)
+//     });
+
+//     const result = await response.json();
+
+//     return result;
+// }
+
+// Second
+// import { request } from '../lib/request';
+
+// const baseUrl = 'http://localhost:3030/jsonstore/games'
+
+// export const getAll = async () => {
+//     const result = await request('GET', baseUrl);
+//     return Object.values(result);
+
+// };
+
+// export const create = async (gameData) => {
+//     const result = await request('POST', baseUrl, gameData);
+
+//     return result;
+// }
+
+//  Third
+import * as request from '../lib/request';
 
 const baseUrl = 'http://localhost:3030/jsonstore/games'
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
     return Object.values(result);
 
 };
 
 export const create = async (gameData) => {
-    const response = await fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(gameData)
-    });
-
-    const result = await response.json();
+    const result = await request.post(baseUrl, gameData);
 
     return result;
 }
