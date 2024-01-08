0ce5e0996560b0d7cc05f28201818aab808f8561
Violeta Kalcheva <violetakalcheva@abv.bg>
2024-01-08 15:58:02 +0200

    Get all games service method and abstract requester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,9 +1,17 @@
-const baseUrl = 'http://localhost:3030/jsonstore'
+import { request } from '../lib/request';
+
+const baseUrl = 'http://localhost:3030/jsonstore/games'
+
+export const getAll = async () => {
+    const result = await request('GET', baseUrl);
+    return Object.values(result);
+
+};
 
 export const create = async (gameData) => {
-    const response = await fetch(`${baseUrl}/games`, {
-        method:'POST',
-        headers:{
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify(gameData)
