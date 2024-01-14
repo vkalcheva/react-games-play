41691a867f3bde8b225e87bf19e7be5449dd23cc
Violeta Kalcheva <violetakalcheva@abv.bg>
2024-01-14 13:06:41 +0200

    Add comment to game by id

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,9 +3,15 @@ import * as request from '../lib/request';
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
 
-export const getAll = async () => {
+export const getAll = async (gameId) => {
+    const query = new URLSearchParams({
+        where: `gameId="${gameId}"`
+    });
+
     const result = await request.get(baseUrl);
-    return Object.values(result);
+
+    // TODO: temp solution until migration to collections service!
+    return Object.values(result).filter(comment => comment.gameId === gameId);
 };
 
 export const create = async (gameId, username, text) => {
