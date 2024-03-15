dea52e0de526ff67448ee5dda7c5bb5aa0f252f9
Violeta Kalcheva <violetakalcheva@abv.bg>
2024-03-15 16:05:04 +0200

    Add user register

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -8,4 +8,9 @@ export const login = async (email, password) => {
         password,
     });
     return result;
-}
+};
+
+export const register = (email, password) => request.post(`${baseUrl}/register`, {
+    email,
+    password,
+});
